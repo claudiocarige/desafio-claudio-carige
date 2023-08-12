514b9c81046ea0b902df915201a92d6f841aee33
Claudio Carige <ccarige@gmail.com>
2023-08-12 10:45:23 -0300

    Create CardapioController Class

---
+++ src/main/java/com/claudiocarige/desafioStartDB/resource/ProdControler.java
@@ -0,0 +1,11 @@
+package com.claudiocarige.desafioStartDB.resource;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/cardapio")
+public class ProdControler {
+
+
+}
