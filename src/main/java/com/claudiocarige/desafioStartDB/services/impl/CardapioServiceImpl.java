d7d2eadae3b28f7e7e5f9aef6104535041bb3eb1
Claudio Carige <ccarige@gmail.com>
2023-08-12 10:56:15 -0300

    Created the findById Method

--- src/main/java/com/claudiocarige/desafioStartDB/services/impl/CardapioServiceImpl.java
+++ src/main/java/com/claudiocarige/desafioStartDB/services/impl/CardapioServiceImpl.java
@@ -1,18 +1,23 @@
 package com.claudiocarige.desafioStartDB.services.impl;
 
 import com.claudiocarige.desafioStartDB.models.Cardapio;
+import com.claudiocarige.desafioStartDB.repositories.CardapioRepository;
 import com.claudiocarige.desafioStartDB.services.CardapioService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
 public class CardapioServiceImpl implements CardapioService {
 
+    private final CardapioRepository cardapioRepository;
 
     @Override
     public Cardapio findById(Long id) {
-        return null;
+        Optional<Cardapio> cardapioObject = cardapioRepository.findById(id);
+        return cardapioObject.orElseThrow(() -> new RuntimeException("Objeto não encontrado."));
     }
 
     @Override
