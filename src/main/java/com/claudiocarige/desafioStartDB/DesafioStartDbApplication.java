e72a916c178023bce713725ee5b0508f3390aaae
Claudio Carige <ccarige@gmail.com>
2023-08-12 10:42:17 -0300

    First commit

---
+++ src/main/java/com/claudiocarige/desafioStartDB/DesafioStartDbApplication.java
@@ -0,0 +1,13 @@
+package com.claudiocarige.desafioStartDB;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioStartDbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioStartDbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/claudiocarige/desafioStartDB/DesafioStartDbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.claudiocarige.desafioStartDB;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioStartDbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
