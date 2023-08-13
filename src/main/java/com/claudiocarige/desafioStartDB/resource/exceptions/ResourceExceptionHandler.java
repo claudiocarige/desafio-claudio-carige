97941a3c1e8af9ae0518e10d2f2778fce700306f
Claudio Carige <ccarige@gmail.com>
2023-08-13 00:48:08 -0300

    Created DataIntegrityViolationException exception

--- src/main/java/com/claudiocarige/desafioStartDB/resource/exceptions/ResourceExceptionHandler.java
+++ src/main/java/com/claudiocarige/desafioStartDB/resource/exceptions/ResourceExceptionHandler.java
@@ -1,6 +1,7 @@
 package com.claudiocarige.desafioStartDB.resource.exceptions;
 
-import com.claudiocarige.desafioStartDB.services.NoSuchElementException;
+import com.claudiocarige.desafioStartDB.services.exceptions.DataIntegrityViolationException;
+import com.claudiocarige.desafioStartDB.services.exceptions.NoSuchElementException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -16,5 +17,12 @@ public class ResourceExceptionHandler {
         StandardError error = new StandardError(System.currentTimeMillis(), HttpStatus.NOT_FOUND.value(),
                 "Object not found ", ex.getMessage(), request.getRequestURI());
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(error);
+    }
+
+    @ExceptionHandler(DataIntegrityViolationException.class)
+    public ResponseEntity<StandardError> dataIntegrityViolationException(DataIntegrityViolationException ex, HttpServletRequest request) {
+        StandardError error = new StandardError(System.currentTimeMillis(), HttpStatus.BAD_REQUEST.value(),
+                "Integrity Violation", ex.getMessage(), request.getRequestURI());
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(error);
     }
 }
