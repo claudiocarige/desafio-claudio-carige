eebd43017f0f2c7f21904a7374c29e002e17ed5a
Claudio Carige <ccarige@gmail.com>
2023-08-13 22:14:27 -0300

    Created ItemPedidoRepresentation(DTO) class

---
+++ src/main/java/com/claudiocarige/desafioStartDB/models/representation/ItemPedidoRepresentation.java
@@ -0,0 +1,25 @@
+package com.claudiocarige.desafioStartDB.models.representation;
+
+import com.claudiocarige.desafioStartDB.models.ItemPedido;
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+
+public class ItemPedidoRepresentation {
+    private Long id;
+    private String item;
+    private Integer quantidade;
+    private Long numeroPedido;
+
+    public ItemPedidoRepresentation (ItemPedido obj){
+        this.id = obj.getId();
+        this.item = obj.getItem().getCodigo();
+        this.quantidade = obj.getQuantidade();
+        this.numeroPedido = obj.getPedido().getId();
+
+    }
+}
+
+
