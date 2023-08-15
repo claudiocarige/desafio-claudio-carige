e89fe165dd92b14b3510f9c4c27a6f77fdb6853b
Claudio Carige <ccarige@gmail.com>
2023-08-15 18:21:55 -0300

    Inserted 1 more request to populate the Bank

--- src/main/java/com/claudiocarige/desafioStartDB/services/impl/DBService.java
+++ src/main/java/com/claudiocarige/desafioStartDB/services/impl/DBService.java
@@ -16,6 +16,7 @@ import java.util.Arrays;
 @RequiredArgsConstructor
 public class DBService {
 
+    private final PedidoServiceImpl pedidoService;
     private final ItensCardapioRepository itensCardapioRepository;
     private final PedidoRepository pedidoRepository;
     public void startDB() {
@@ -23,7 +24,7 @@ public class DBService {
         ItensCardapio itensCardapio02 = new ItensCardapio("chantily", "Chantily (extra do Café)", 1.50f, ItemCategoria.EXTRA);
         ItensCardapio itensCardapio03 = new ItensCardapio("suco", "Suco Natural", 6.20f, ItemCategoria.PRINCIPAL);
         ItensCardapio itensCardapio04 = new ItensCardapio("sanduiche", "Sanduíche", 6.50f, ItemCategoria.PRINCIPAL);
-        ItensCardapio itensCardapio05 = new ItensCardapio("queijo", "Queijo (extra do Sanduíche)f", 2.00f, ItemCategoria.EXTRA);
+        ItensCardapio itensCardapio05 = new ItensCardapio("queijo", "Queijo (extra do Sanduíche)", 2.00f, ItemCategoria.EXTRA);
         ItensCardapio itensCardapio06 = new ItensCardapio("salgado", "Salgado", 7.25f, ItemCategoria.PRINCIPAL);
         ItensCardapio itensCardapio07 = new ItensCardapio("combo1", "1 Suco e 1 Sanduíche", 9.50f, ItemCategoria.COMBO);
         ItensCardapio itensCardapio08 = new ItensCardapio("combo2", "1 Café e 1 Sanduíche", 7.50f, ItemCategoria.COMBO);
@@ -32,22 +33,31 @@ public class DBService {
                 ,itensCardapio05, itensCardapio06, itensCardapio07, itensCardapio08));
         Pedido pedido01 = new Pedido();
         Pedido pedido02 = new Pedido();
+        Pedido pedido03 = new Pedido();
 
-        ItemPedido item01 = new ItemPedido(itensCardapio01, 2, pedido01);
-        ItemPedido item02 = new ItemPedido(itensCardapio04, 1, pedido01);
-        ItemPedido item03 = new ItemPedido(itensCardapio07, 2, pedido02);
+        ItemPedido item05 = new ItemPedido(itensCardapio03, 1, pedido01);
+        ItemPedido item06 = new ItemPedido(itensCardapio04, 3, pedido01);
+        ItemPedido item07 = new ItemPedido(itensCardapio01, 1, pedido01);
+        ItemPedido item01 = new ItemPedido(itensCardapio01, 2, pedido03);
+        ItemPedido item02 = new ItemPedido(itensCardapio04, 1, pedido03);
+        ItemPedido item03 = new ItemPedido(itensCardapio04, 2, pedido02);
         ItemPedido item04 = new ItemPedido(itensCardapio03, 2, pedido02);
 
         pedido01.setFormaPagamento(FormaPagamento.DEBITO);
-        pedido01.addItensPedido(item01);
-        pedido01.addItensPedido(item02);
+        pedido01.addItensPedido(item05);
+        pedido01.addItensPedido(item06);
+        pedido01.addItensPedido(item07);
         pedido01.calcularValorPedido();
         pedido02.setFormaPagamento(FormaPagamento.CREDITO);
         pedido02.addItensPedido(item03);
         pedido02.addItensPedido(item04);
         pedido02.calcularValorPedido();
+        pedido03.setFormaPagamento(FormaPagamento.DEBITO);
+        pedido03.addItensPedido(item01);
+        pedido03.addItensPedido(item02);
+        pedido03.calcularValorPedido();
 
-        pedidoRepository.saveAll(Arrays.asList(pedido01,pedido02));
+        pedidoRepository.saveAll(Arrays.asList(pedido01, pedido02, pedido03));
     }
 
 }
