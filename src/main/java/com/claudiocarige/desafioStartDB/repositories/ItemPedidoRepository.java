1ced24ae1941a619ceadba39eb3ecf292ad3d6be
Claudio Carige <ccarige@gmail.com>
2023-08-17 01:17:38 -0300

    Created ItemPedidoRepository class

---
+++ src/main/java/com/claudiocarige/desafioStartDB/repositories/ItemPedidoRepository.java
@@ -0,0 +1,8 @@
+package com.claudiocarige.desafioStartDB.repositories;
+
+import com.claudiocarige.desafioStartDB.models.ItemPedido;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ItemPedidoRepository extends JpaRepository<ItemPedido, Integer> {
+
+}
