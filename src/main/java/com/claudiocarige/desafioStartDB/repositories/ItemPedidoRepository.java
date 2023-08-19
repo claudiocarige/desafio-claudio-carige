a7082117c4e586b209c5914cc9111170ca801f2c
Claudio Carige <ccarige@gmail.com>
2023-08-18 23:38:56 -0300

    findItemByCodigo method created to search item in ItensCardapio

--- src/main/java/com/claudiocarige/desafioStartDB/repositories/ItemPedidoRepository.java
+++ src/main/java/com/claudiocarige/desafioStartDB/repositories/ItemPedidoRepository.java
@@ -4,5 +4,4 @@ import com.claudiocarige.desafioStartDB.models.ItemPedido;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ItemPedidoRepository extends JpaRepository<ItemPedido, Integer> {
-
 }
